8914cb1d0ddf08bde4af0d0aa5ec489bd4fec052
Sean O'Dowd <sdo2003@gmail.com>
2021-09-19 12:57:02 -0400

    Adding click handling to iOS app

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -9,60 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var tf1: UITextField!
-    var label: UILabel!
-    var b1: UIButton!
+    //@IBOutlet weak var sc1: UISegmentedControl!
+    @IBOutlet weak var iv1: UIImageView!
+    @IBOutlet weak var pc1: UIPageControl!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        // code to display Textfield on UI
-        tf1 = UITextField()
-        tf1.frame = CGRect(x: 20, y: 100, width: 200, height: 40)
-        
-        tf1.text = ""
-        tf1.textColor = .black
-        tf1.textAlignment = .center
-        tf1.keyboardType = .default
-        tf1.returnKeyType = .done
-        tf1.isSecureTextEntry = true
-        tf1.backgroundColor = .yellow
-        
-        self.view.addSubview(tf1)
-        
-        //code to display Label
-        label = UILabel()
-        label.frame = CGRect(x: 20, y: 180, width: 200, height: 40)
-        
-        label.textColor = .white
-        label.backgroundColor = .blue
-        label.textAlignment = .center
-        label.numberOfLines = 1
-        
-        self.view.addSubview(label)
-        
-        //code to display Button
-        b1 = UIButton()
-        b1.frame = CGRect(x: 20, y: 220, width: 200, height: 40)
-        
-        b1.setTitle("Click Here", for: .normal)
-        b1.backgroundColor = .red
-        b1.setTitleColor(.white, for: .normal)
-        
-        b1.addTarget(self, action: #selector(click1), for: .touchUpInside)
-        
-        self.view.addSubview(b1)
-        
-        self.view.backgroundColor = .darkGray
-    }
-    
-    @objc func click1() {
-        
-        label.text = tf1.text
-        
-    }
+        let image: UIImage = UIImage(imageLiteralResourceName: "lion.jpg")
+        iv1.image = image
 
 
+    }
+
+    @IBAction func clickPageControl(_ sender: Any) {
+        let index: Int = pc1.currentPage
+        if index == 0 {
+            let image: UIImage = UIImage(imageLiteralResourceName: "lion.jpg")
+            iv1.image = image
+        }
+        
+        if index == 1 {
+            let image: UIImage = UIImage(imageLiteralResourceName: "tiger.jpg")
+            iv1.image = image
+        }
+        
+        if index == 2 {
+            let image: UIImage = UIImage(imageLiteralResourceName: "bear.jpg")
+            iv1.image = image
+        }
+    }
+    //    @IBAction func clickSegmentedControl(_ sender: Any) {
+//        let index: Int = sc1.selectedSegmentIndex
+//        if index == 0 {
+//            let image: UIImage = UIImage(imageLiteralResourceName: "lion.jpg")
+//            iv1.image = image
+//        }
+//
+//        if index == 1 {
+//            let image: UIImage = UIImage(imageLiteralResourceName: "tiger.jpg")
+//            iv1.image = image
+//        }
+//
+//        if index == 2 {
+//            let image: UIImage = UIImage(imageLiteralResourceName: "bear.jpg")
+//            iv1.image = image
+//        }
+//    }
 }
-

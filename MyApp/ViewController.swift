c7b184ed23f5b1a868fc63be6f778b6bd0f220c0
Sean O'Dowd <sod2003@gmail.com>
2021-09-21 15:49:19 -0400

    importing WebKit and sending URLRequests

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -6,40 +6,36 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var pv1: UIProgressView!
+    @IBOutlet weak var tv1: UITextField!
     
-    var iv1: UIImageView!
+    @IBOutlet weak var button1: UIButton!
     
-    var timer1: Timer!
+    @IBOutlet weak var wv1: WKWebView!
+    
+    var url1: URL! // to assign a URL
+    
+    var request1: URLRequest! //to create a request
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
             
-        timer1 = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateProgress), userInfo: nil, repeats: true)
-
-    }
-
-    @objc func updateProgress() {
-        pv1.progress += 0.2
         
-        if pv1.progress == 1.0 {
-            
-            pv1.isHidden = true
-            
-            iv1 = UIImageView()
-            iv1.frame = CGRect(x: 30, y: 100, width: 150, height: 150)
-            iv1.contentMode = .scaleAspectFit
-            iv1.backgroundColor = .black
-            iv1.image = UIImage(named: "lion.jpg")
-            
-            self.view.addSubview(iv1)
-            
-            
-        }
+
     }
+
+    @IBAction func clickButton(_ sender: Any) {
+        url1 = URL(string: tv1.text!)
+        
+        request1 = URLRequest(url: url1)
+        
+        wv1.load(request1)
+    }
+    
    
 }

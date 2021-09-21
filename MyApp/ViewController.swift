58fd5b7553d23b58b0819f331e063d7c5a151123
Sean O'Dowd <sod2003@gmail.com>
2021-09-21 14:21:42 -0400

    Progress bar and timer for update

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -9,34 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var iv1: UIImageView!
-    @IBOutlet weak var pc1: UIPageControl!
+    @IBOutlet weak var pv1: UIProgressView!
     
-
+    var iv1: UIImageView!
+    
+    var timer1: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let image: UIImage = UIImage(imageLiteralResourceName: "lion.jpg")
-        iv1.image = image
-
+            
+        timer1 = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateProgress), userInfo: nil, repeats: true)
 
     }
 
-    @IBAction func clickPageControl(_ sender: Any) {
-        let index: Int = pc1.currentPage
-        if index == 0 {
-            let image: UIImage = UIImage(imageLiteralResourceName: "lion.jpg")
-            iv1.image = image
-        }
+    @objc func updateProgress() {
+        pv1.progress += 0.2
         
-        if index == 1 {
-            let image: UIImage = UIImage(imageLiteralResourceName: "tiger.jpg")
-            iv1.image = image
-        }
-        
-        if index == 2 {
-            let image: UIImage = UIImage(imageLiteralResourceName: "bear.jpg")
-            iv1.image = image
+        if pv1.progress == 1.0 {
+            
+            pv1.isHidden = true
+            
+            iv1 = UIImageView()
+            iv1.frame = CGRect(x: 30, y: 100, width: 150, height: 150)
+            iv1.contentMode = .scaleAspectFit
+            iv1.backgroundColor = .black
+            iv1.image = UIImage(named: "lion.jpg")
+            
+            self.view.addSubview(iv1)
+            
+            
         }
     }
    

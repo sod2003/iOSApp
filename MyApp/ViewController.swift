afe898a79a9b351aab1f6afdf5f942ab6883d702
Sean O'Dowd <sod2003@gmail.com>
2021-09-27 14:21:15 -0400

    Implementing imagepicker for photo library and camera

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -7,70 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBOutlet weak var tf1: UITextField!
-    @IBOutlet weak var tf2: UITextField!
+    @IBOutlet weak var iv1: UIImageView!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
     
-    var pv1: UIPickerView!
-    
-    var countryNamesArray: [String] = []
-    
-    var dp1: UIDatePicker!
-    var dateFormatter1: DateFormatter!
+    var PhotoLibraryImagePicker1: UIImagePickerController!
+    var CameraImagePicker2: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        countryNamesArray = ["USA", "UK", "India", "Australia", "UAE", "China"]
         
-        //code for pickerview
-        pv1 = UIPickerView()
-        pv1.backgroundColor = .darkGray
-        tf1.inputView = pv1 //assigns pickerview to TextField's Keyboard Input View
-        
-        pv1.delegate = self
-        pv1.dataSource = self
-        
-        //code for datepicker
-        dp1 = UIDatePicker()
-        dp1.backgroundColor = .gray
-        dp1.datePickerMode = .dateAndTime
-        dp1.addTarget(self, action: #selector(dp1Click), for: .valueChanged)
-        tf2.inputView = dp1
         
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return countryNamesArray.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return countryNamesArray[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        tf1.text = countryNamesArray[row]
-    }
-    
-    // Date Picker Function
-    @objc func dp1Click() {
-        dateFormatter1 = DateFormatter()
+    @IBAction func photoClick(_ sender: UIButton) {
         
-        dateFormatter1.dateStyle = .medium
-        dateFormatter1.timeStyle = .medium
+        // Access Photo Library
+        PhotoLibraryImagePicker1 = UIImagePickerController()
         
-        tf2.text = dateFormatter1.string(from: dp1.date)
+        PhotoLibraryImagePicker1.delegate = self
+        PhotoLibraryImagePicker1.sourceType = .photoLibrary
+        PhotoLibraryImagePicker1.allowsEditing = true
+        
+        self.present(PhotoLibraryImagePicker1, animated: true, completion: nil)
+        
     }
     
-    //Keyboard Hiding
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        tf1.resignFirstResponder()
-        tf2.resignFirstResponder()
+    @IBAction func cameraClick(_ sender: UIButton) {
+        
+        // Access Camera
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera)) {
+            CameraImagePicker2 = UIImagePickerController()
+            
+            CameraImagePicker2.delegate = self
+            CameraImagePicker2.sourceType = .camera
+            CameraImagePicker2.cameraDevice = .rear
+            CameraImagePicker2.allowsEditing = false
+            
+            self.present(CameraImagePicker2, animated: true, completion: nil)
+    
+        } else {
+            print("Camera not found")
+        }
+    }
+    
+    // Implementing UIImagePickerControllerDelegate protocol methods
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if picker == PhotoLibraryImagePicker1 {
+            print("image info --> \(info)")
+            
+            let image1 = info [.editedImage] as! UIImage
+            iv1.image = image1
+            self.dismiss(animated: true, completion: nil)
+            
+        } else {
+            print("image info --> \(info)")
+            
+            let image2 = info [.originalImage] as! UIImage
+            iv1.image = image2
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 }

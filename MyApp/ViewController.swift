1de5b109a09f6ce49a407bd65d5489fb6243d282
Sean O'Dowd <sod2003@gmail.com>
2021-09-27 12:44:17 -0400

    Implemented pickerview and datepicker with textfield views

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -7,30 +7,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    @IBOutlet weak var button1: UIButton!
-    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var tf1: UITextField!
+    @IBOutlet weak var tf2: UITextField!
     
-    var dateAndTimeInfo: Date!
+    var pv1: UIPickerView!
+    
+    var countryNamesArray: [String] = []
+    
+    var dp1: UIDatePicker!
     var dateFormatter1: DateFormatter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        countryNamesArray = ["USA", "UK", "India", "Australia", "UAE", "China"]
+        
+        //code for pickerview
+        pv1 = UIPickerView()
+        pv1.backgroundColor = .darkGray
+        tf1.inputView = pv1 //assigns pickerview to TextField's Keyboard Input View
+        
+        pv1.delegate = self
+        pv1.dataSource = self
+        
+        //code for datepicker
+        dp1 = UIDatePicker()
+        dp1.backgroundColor = .gray
+        dp1.datePickerMode = .dateAndTime
+        dp1.addTarget(self, action: #selector(dp1Click), for: .valueChanged)
+        tf2.inputView = dp1
         
     }
     
-    @IBAction func clickButton(_ sender: UIButton) {
-        dateAndTimeInfo = Date()
-        
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countryNamesArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countryNamesArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        tf1.text = countryNamesArray[row]
+    }
+    
+    // Date Picker Function
+    @objc func dp1Click() {
         dateFormatter1 = DateFormatter()
         
-        dateFormatter1.timeStyle = .short
-        dateFormatter1.dateStyle = .short
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .medium
         
-        label1.text = dateFormatter1.string(from: dateAndTimeInfo as Date)
+        tf2.text = dateFormatter1.string(from: dp1.date)
     }
     
+    //Keyboard Hiding
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        tf1.resignFirstResponder()
+        tf2.resignFirstResponder()
+    }
 }

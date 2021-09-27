fb99d280cc3a329ca96b81d3b70bb78bdbf2fca7
Sean O'Dowd <sod2003@gmail.com>
2021-09-27 10:23:14 -0400

    Implementing Date and DateFormatter

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -7,56 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController {
     
-    @IBOutlet weak var pv1: UIPickerView!
+    @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var label1: UILabel!
     
-    var component1Array: [String] = []
-    var component2Array: [String] = []
+    var dateAndTimeInfo: Date!
+    var dateFormatter1: DateFormatter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        component1Array = ["One", "Two", "Three", "Four", "Five"]
-        
-        component2Array = ["Yellow", "Blue", "Green", "Orange"]
-        
-        pv1.delegate = self
-        pv1.dataSource = self
         
     }
     
-    // Number of components in PickerView
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
+    @IBAction func clickButton(_ sender: UIButton) {
+        dateAndTimeInfo = Date()
+        
+        dateFormatter1 = DateFormatter()
+        
+        dateFormatter1.timeStyle = .short
+        dateFormatter1.dateStyle = .short
+        
+        label1.text = dateFormatter1.string(from: dateAndTimeInfo as Date)
     }
     
-    // Number of rows in a component
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if component == 0 {
-            return component1Array.count
-        } else {
-            return component2Array.count
-        }
-    }
-    
-    // Display the information in rows
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if component == 0 {
-            return component1Array[row]
-        } else {
-            return component2Array[row]
-        }
-    }
-    
-    // When user selects any row in a component
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if component == 0 {
-            label1.text = component1Array[row]
-        } else {
-            label1.text = component2Array[row]
-        }
-    }
 }

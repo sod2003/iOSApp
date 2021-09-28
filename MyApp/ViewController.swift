ce4e8bb5d8a6d18d50a09f4e07fea07259ef2fcf
Sean O'Dowd <sod2003@gmail.com>
2021-09-28 09:44:05 -0400

    Implementing AVKit and using  AVPlayerViewController

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -6,39 +6,29 @@
 //
 
 import UIKit
-import QuartzCore
+import AVKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var tf1: UITextField!
-    @IBOutlet weak var label1: UILabel!
-    
-    var layer1: CALayer!
-    var layer2: CALayer!
-    var layer3: CALayer!
+    @IBOutlet weak var button1: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        layer1 = tf1.layer
-        
-        layer1.cornerRadius = 20
-        layer1.borderColor = UIColor.black.cgColor
-        layer1.borderWidth = 2
-        
-        layer2 = label1.layer
-        
-        layer2.cornerRadius = 10
-        layer2.borderColor = UIColor.red.cgColor
-        layer2.borderWidth = 2
-        
-        layer3 = self.view.layer
-        
-        layer3.borderColor = UIColor.yellow.cgColor
-        layer3.borderWidth = 5
-        layer3.cornerRadius = 10
         
     }
     
+    @IBAction func buttonClick(_ sender: UIButton) {
+        let videoURL = URL(string: "http://www.ebookfrenzy.com/ios_book/movie/movie.mov")
+
+        let player = AVPlayer(url: videoURL!)
+
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+
+        self.present(playerViewController, animated: true)
+
+        playerViewController.player!.play()
+    }
 }
